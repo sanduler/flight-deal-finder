286bbee1c6892dd6e71ab66bfdb47e1e7bdd2324
Ruben Sanduleac <ruvim.sanduleac@gmail.com>
2022-02-25 08:56:26 -0800

    implemented the FlightSearch class to be able to GET tequila info

--- flight_search.py
+++ flight_search.py
@@ -1,10 +1,30 @@
 # Name: Ruben Sanduleac
 # Date: 02/24/22
 # Description: This class is responsible for talking to the Flight Search API.
+
+import os
+import requests
+TEQUILA_ENDPOINT = "https://tequila-api.kiwi.com"
+TEQUILA_API = os.environ["TEQUILA_API_KEY"]
+
+
 class FlightSearch:
-    def __init__(self):
-        pass
+    # def __init__(self):
+    #     self.get_destination_code()
 
     def get_destination_code(self, city_name):
-        code = "Testing"
+        location_endpoint = f"{TEQUILA_ENDPOINT}/locations/query"
+        header = {
+            "apikey": TEQUILA_API,
+        }
+        query = {
+            "term": city_name,
+            "location_types": "city",
+        }
+
+        response = requests.get(url=location_endpoint, params=query, headers=header)
+        response.raise_for_status()
+        # print(response)
+        destination_data = response.json()["locations"]
+        code = destination_data[0]["code"]
         return code
