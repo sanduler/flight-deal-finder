448994180da0681d2b6a1988654a1ee860cdae90
Ruben Sanduleac <ruvim.sanduleac@gmail.com>
2022-02-25 12:23:21 -0800

    implemented the Flight Data class and created the for loop for looking up airport city

--- flight_data.py
+++ flight_data.py
@@ -2,4 +2,10 @@
 # Date: 02/24/22
 # Description: This class is responsible for structuring the flight data.
 class FlightData:
-    pass
+    def __int__(self, price, origin_city, origin_airport, destination_city, destination_airport, out_date, return_date):
+        self.price = price
+        self.origin_city = origin_city
+        self.destination_city = destination_city
+        self.destination_airport = destination_airport
+        self.out_date = out_date
+        self.return_date = return_date
--- main.py
+++ main.py
@@ -5,11 +5,12 @@
 
 from data_manager import DataManager
 from flight_search import FlightSearch
+from datetime import datetime, timedelta
+
+ORIGIN_CITY ="PDX"
 
 data_managing = DataManager()
 sheet_data = data_managing.get_destination_data()
-# print(sheet_data)
-
 flight_searching = FlightSearch()
 
 if sheet_data[0]["iataCode"] == "":
@@ -20,3 +21,12 @@ if sheet_data[0]["iataCode"] == "":
     data_managing.destination_data = sheet_data
     data_managing.update_destination_codes()
 
+
+# start the search from next day --> tomorrow
+tomorrow = datetime.now() + timedelta(days=1)
+# look into the future six months --> 6 months = 30 * 6 months
+six_months = datetime.now() + timedelta(days=(6*30))
+
+# look into each of the destination codes to search for the flight
+for destination in sheet_data:
+    pass
