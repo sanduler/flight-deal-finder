974e7f98f4d1acb3ee5cd2aad449ef7f835c5181
Ruben Sanduleac <ruvim.sanduleac@gmail.com>
2022-02-27 22:46:38 -0800

    added the comments for the notification manager to describe the functionality.

--- notification_manager.py
+++ notification_manager.py
@@ -12,10 +12,16 @@ TWILIO_VERIFIED_NUMBER = os.environ["TO_TEXT"]
 
 
 class NotificationManager:
+    """This class is responsible for notifying the user
+    The class uses Twilio API to send the user a text about the
+     lowest flight deal to date."""
     def __init__(self):
+        """Prototype which always initializes the token and the if from Twilio"""
         self.client = Client(TWILIO_SID, TWILIO_AUTH_TOKEN)
 
     def send_sms(self, message):
+        """send_sms function is responsible for the message that is sent to the user from a varified
+         phone number to a verified phone"""
         message = self.client.messages.create(
             body=message,
             from_=TWILIO_VIRTUAL_NUMBER,
